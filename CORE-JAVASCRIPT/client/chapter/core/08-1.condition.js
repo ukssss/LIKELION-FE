629de72e9620c69a8437b2e6f3b143b9dc6a6bde
ukssss <1872003115su@gmail.com>
2022-12-29 13:14:47 +0900

    [✨ add] 08-1.condition.js 실습

--- CORE-JAVASCRIPT/client/chapter/core/08-1.condition.js
+++ CORE-JAVASCRIPT/client/chapter/core/08-1.condition.js
@@ -14,16 +14,30 @@ console.log(result);
 //       Yes | No
 
 // 영화 봤니?
-let didWatchMovie;
+let didWatchMovie = "yes";
 
 // 영화 볼거니?
-let goingToWatchMovie;
+let goingToWatchMovie = "no";
 
 // if 문(statement)
-
-// else 절(caluse)
+if (didWatchMovie === "yes") {
+  console.log("영화 봤어!");
+}
 
 // else if 복수 조건 처리
+else if (goingToWatchMovie === "yes") {
+  console.log("영화 볼 예정이야!");
+}
+
+// else 절(caluse)
+else {
+  console.log("영화 안볼래!");
+}
+
+// 삼항연산자 식
+let mention = "";
+didWatchMovie === "yes" ? (mention = "봤음") : goingToWatchMovie === "yes" ? (mention = "볼거임") : (mention = "안봐");
+console.log(mention);
 
 // 조건부 연산자
 
