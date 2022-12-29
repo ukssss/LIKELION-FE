a827ed71a1ac1d28c95453cd6dfde7ab38382e30
ukssss <1872003115su@gmail.com>
2022-12-29 11:51:59 +0900

    [✨ add] 08-1.condition.js 실습, 자바스크립트 공식 이름 문제 해결

---
+++ CORE-JAVASCRIPT/client/chapter/core/08-1.condition.js
@@ -0,0 +1,30 @@
+/* ---------------------------------------------------------------------- */
+/* Condition                                                              */
+/* ---------------------------------------------------------------------- */
+let answer = prompt("자바스크립트의 '공식'이름은 무엇일까요 ?");
+let result = "";
+answer === "ECMAScript" ? (result += "정답") : (result += "바보");
+console.log(result);
+
+// 그 영화 봤니?
+//     ↓
+// Yes | No
+//     | 영화 볼거니?
+//           ↓
+//       Yes | No
+
+// 영화 봤니?
+let didWatchMovie;
+
+// 영화 볼거니?
+let goingToWatchMovie;
+
+// if 문(statement)
+
+// else 절(caluse)
+
+// else if 복수 조건 처리
+
+// 조건부 연산자
+
+// 멀티 조건부 연산자 식
