115cd5160d2576dba8b4484cd667e46efe26ed63
ukssss <1872003115su@gmail.com>
2022-12-30 15:12:03 +0900

    [✨ add] 09-4.loop (for ~ in) 실습

--- CORE-JAVASCRIPT/client/chapter/core/09-4.loop.js
+++ CORE-JAVASCRIPT/client/chapter/core/09-4.loop.js
@@ -7,20 +7,29 @@ const javaScript = {
   createAt: "1995.05",
   standardName: "ECMAScript",
   currentVersion: 2022,
+  hasOwnProperty: function () {
+    return "헤헤";
+  },
 };
 
 let key = "valueOf";
 
 // console.log(key in javaScript);
 
-// 객체의 속성(property) 포함 여부 확인 방법
-// - 모든 객체가 사용 가능하도록 속성이 확장되었을 때 포함 여부 결과는?
+// TODO 객체의 속성(property) 포함 여부 확인 방법
+// TODO 모든 객체가 사용 가능하도록 속성이 확장되었을 때 포함 여부 결과는?
+// console.log(javaScript.hasOwnProperty(key));
+Object.prototype.nickName = "ukss";
 
-console.log(javaScript.hasOwnProperty(key)
+// TODO 객체 자신의 속성인지 확인하는 방법
+// TODO "자신의 속성을 가지고있는지 확인 방법"이 덮어쓰여질 수 있는 위험에 대처하는 안전한 방법은?
+console.log(Object.prototype.hasOwnProperty.call(javaScript, key));
 
-// 객체 자신의 속성인지 확인하는 방법
-// - "자신의 속성을 가지고있는지 확인 방법"이 덮어쓰여질 수 있는 위험에 대처하는 안전한 방법은?
-
-// for ~ in 문
+// * for ~ in 문
+for (let key in javaScript) {
+  if ({}.hasOwnProperty.call(javaScript, key)) {
+    console.log(key);
+  }
+}
 // - 객체 자신의 속성만 순환하려면?
 // - 배열 객체 순환에 사용할 경우?
