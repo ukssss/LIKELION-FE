8313ba965424a0d2f292c057ddb398aa5e1bddbe
ukssss <1872003115su@gmail.com>
2023-01-02 20:05:44 +0900

    2023_01_02) MBTI 버튼 클릭으로 인해 문제 전환 구현

--- MBTI/index.js
+++ MBTI/index.js
@@ -1,6 +1,14 @@
+let num = 1;
+const q = {
+  1: { title: "문제 1번", type: "EI", A: "E", B: "I" },
+  2: { title: "문제 2번", type: "EI", A: "E", B: "I" },
+  3: { title: "문제 3번", type: "EI", A: "E", B: "I" },
+};
+
 function start() {
   $(".start").hide();
   $(".question").show();
+  next();
 }
 
 $("#A").click(function () {
@@ -8,4 +16,17 @@ $("#A").click(function () {
   let preValue = $("#" + type).val();
 
   $("#" + type).val(parseInt(preValue) + 1);
+  next();
 });
+
+$("#B").click(function () {
+  next();
+});
+
+function next() {
+  $("#title").html(q[num]["title"]);
+  $("#type").val(q[num]["type"]);
+  $("#A").html(q[num]["A"]);
+  $("#B").html(q[num]["B"]);
+  num++;
+}
