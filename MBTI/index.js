17a99d826a8029cb38ca2304309faf3139d44d76
ukssss <1872003115su@gmail.com>
2023-01-02 19:58:49 +0900

    2023_01_02) MBTI 버튼 클릭 이벤트 구현

--- MBTI/index.js
+++ MBTI/index.js
@@ -2,3 +2,10 @@ function start() {
   $(".start").hide();
   $(".question").show();
 }
+
+$("#A").click(function () {
+  let type = $("#type").val();
+  let preValue = $("#" + type).val();
+
+  $("#" + type).val(parseInt(preValue) + 1);
+});
