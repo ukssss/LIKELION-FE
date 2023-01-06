9ba6a6336e9675b58b7e812453fbfd8c29fbee49
ukssss <1872003115su@gmail.com>
2023-01-06 15:51:56 +0900

    2023_01_06) 1.traversal.js 및 dom/getNode.js 실습

---
+++ CORE-JAVASCRIPT/client/chapter/dom/01.traversal.js
@@ -0,0 +1,53 @@
+/* ---------------------------------------------------------------------- */
+/* DOM traversal                                                          */
+/* ---------------------------------------------------------------------- */
+
+/* 모든 노드에서 사용 */
+// - parentNode
+// - childNodes
+// - firstChild
+// - lastChild
+// - previousSibling
+// - nextSibling
+
+/* 요소 노드에서만 사용 가능 */
+// - parentElement
+// - children
+// - firstElementChild
+// - lastElementChild
+// - previousElementSibling
+// - nextElementSibling
+
+/* 문서 대상 찾기 */
+// - getElementById
+// - getElementsByTagName
+// - getElementsByClassName
+// - querySelector
+// - querySelectorAll
+// - closest
+
+// let first = document.querySelector(".first"); // ? <span class="first">hello</span>
+let [first, second] = document.querySelectorAll("span"); // ? NodeList(2) [span.first, span.second]
+
+// console.log(second);
+
+/* 문서 대상 확인 */
+// - matches
+// * 선택자 안에 class || id 를 가지고 있는 대상이 있어?
+console.log(getNode(".first").matches(".first"));
+
+// - contains
+// * 선택자의 자식들 중에 해당 element 가 있어?
+console.log(getNode("h1").contains(getNode(".first")));
+
+let first = getNode(".first");
+
+let clicked = false;
+document.addEventListener("click", () => {
+  if (first.classList.contains("first") && !clicked) {
+    first.classList.add("is-active");
+  } else {
+    first.classList.remove("is-active");
+  }
+  clicked = !clicked;
+});
