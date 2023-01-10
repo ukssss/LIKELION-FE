09a0f5559d20b08ffbec774e185b5b69d7a239bc
ukssss <1872003115su@gmail.com>
2023-01-10 15:33:38 +0900

    2023_01_10) 03.attr IIFE 패턴 추가

--- CORE-JAVASCRIPT/client/lib/dom/attr.js
+++ CORE-JAVASCRIPT/client/lib/dom/attr.js
@@ -38,3 +38,47 @@ function attr(node, prop, value) {
 
   return !value ? getAttr(node, prop) : setAttr(node, prop, value);
 }
+
+// * IIFE
+// const attrIIFE = (function () {
+//   // * get 함수 만들기
+//   function getAttr(node, prop) {
+//     if (typeof node === "string") {
+//       node = getNode(node);
+//     }
+//     //node = document.querySelector(".first");
+
+//     return node.getAttribute(prop);
+//   }
+
+//   // * set 함수 만들기
+//   // computed property
+//   function setAttr(node, prop, value) {
+//     // validation : 확인
+//     if (typeof node === "string") node = getNode(node);
+//     if (typeof prop !== "string") throw new TypeError("setAttr 함수의 두 번째 인자는 문자 타입이어야 합니다 ");
+
+//     if (prop.includes("data")) {
+//       let rest = prop.slice(5);
+//       node.dataset[rest] = value;
+//     }
+
+//     if (!value) throw new SyntaxError("setAttr 함수의 세 번째 인자는 필수값입니다");
+//     node.setAttribute(prop, value);
+//   }
+
+//   // first 노드 안에 data-value 의 값으로 hello 를 집어넣었다
+
+//   // * common 함수 만들기
+//   function attr(node, prop, value) {
+//     // if (!value) {
+//     //   return getAttr(node, prop);
+//     // } else {
+//     //   setAttr(node, prop, value);
+//     // }
+
+//     return !value ? getAttr(node, prop) : setAttr(node, prop, value);
+//   }
+
+//   return attrIIFE;
+// })();
