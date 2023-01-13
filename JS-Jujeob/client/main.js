0152842f79afbdd4bfdb53756f26588bc7a26dec
ukssss <1872003115su@gmail.com>
2023-01-13 15:03:17 +0900

    2023_01_13) Jujeob 주접 생성기 제작 및 랜덤함수 제작

--- JS-Jujeob/client/main.js
+++ JS-Jujeob/client/main.js
@@ -1,1 +1,18 @@
-console.log('aa');
+import { jujeobData } from "./data/data.js";
+import { getInputValue, getNode, getRandom } from "./lib/index.js";
+
+let a = 1;
+
+const submit = getNode("#submit");
+
+console.log(submit);
+
+function clickSubmitHandler(e) {
+  e.preventDefault();
+  let name = getInputValue("#nameField");
+  let list = jujeobData(name);
+
+  console.log(list[getRandom(list.length - 1)]);
+}
+
+submit.addEventListener("click", clickSubmitHandler);
