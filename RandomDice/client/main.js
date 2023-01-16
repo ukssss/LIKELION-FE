a56e45128e0c02d2716f58d7e3015d16cac1abbf
ukssss <1872003115su@gmail.com>
2023-01-16 11:32:26 +0900

    2023_01_16) RandomDice 버튼 구조 분해 할당 사용

--- RandomDice/client/main.js
+++ RandomDice/client/main.js
@@ -1,28 +1,43 @@
-import { diceAnimation, getNode } from "./lib/index.js";
+import { diceAnimation, getNode, getNodes } from "./lib/index.js";
 
 // [주사위 굴리기]
-// 1. dice 애니메이션 불러오기
+// 1. dice 애니메이션 불러오기 ✅
 // 2. bindEvent 유틸 함수 만들기
 // 3. handleRollingDice 함수 만들고 토글로 애니메이션 제작
 // 4. 변수 보호를 위한 클로저 + IIFE 사용하기
 
-const rollingDiceButton = getNode(".buttonGroup > button:nth-child(1)");
+// [레코드 리스트 보이기]
+// 1. handleRecord 함수를 만들기
+// 2. disable 활성 유틸 함수 만들기
+// 3. handleReset 함수를 만들기
+// 4. visible 활성 유틸 함수 만들기
+// 5. toggleState 유틸 함수 만들기
 
-const handleRollingDice = () => {
+// 배열의 구조 분해 할당
+const [rollingDiceButton, recordButton, resetButton] = getNodes(".buttonGroup > button");
+
+// const rollingDiceButton = getNode(".buttonGroup > button:nth-child(1)");
+// const recordButton = getNode(".buttonGroup > button:nth-child(2)");
+// const resetButton = getNode(".buttonGroup > button:nth-child(3)");
+
+// IIFE
+const handlerRollingDice = (() => {
   let stopAnimation;
   let isRolling = false;
 
   return () => {
     if (!isRolling) {
       stopAnimation = setInterval(diceAnimation, 1000);
+      recordButton.disabled = true;
       console.log("애니메이션 시작");
     } else {
       clearInterval(stopAnimation);
+      recordButton.disabled = false;
       console.log("애니메이션 중지");
     }
 
     isRolling = !isRolling;
   };
-};
+})();
 
-rollingDiceButton.addEventListener("click", handleRollingDice());
+rollingDiceButton.addEventListener("click", handlerRollingDice);
