141cc2e0246fe9a7918a12fcc59d8c1609164e83
ukssss <1872003115su@gmail.com>
2023-01-16 09:45:24 +0900

    2023_01_16) Jujeob copy.js 추가 및 shake 구현

--- JS-Jujeob/client/main.js
+++ JS-Jujeob/client/main.js
@@ -1,5 +1,5 @@
 import { jujeobData } from "./data/data.js";
-import { clearContents, getInputValue, getNode, getRandom, insertLast, isNumericString, showAlert } from "./lib/index.js";
+import { clearContents, getInputValue, getNode, getRandom, insertLast, isNumericString, showAlert, copy, addClass, removeClass } from "./lib/index.js";
 
 const submit = getNode("#submit");
 const resultArea = getNode(".result");
@@ -9,12 +9,16 @@ function clickSubmitHandler(e) {
   let name = getInputValue("#nameField");
 
   if (!name) {
-    showAlert(".alert", "이름을 입력해주세요", 3000);
+    showAlert(".alert", "이름을 입력해주세요", 2000);
+    addClass(resultArea, "shake");
+    setTimeout(() => {
+      removeClass(resultArea, "shake");
+    }, 1000);
     return;
   }
 
   if (isNumericString(name)) {
-    showAlert(".alert", "제대로된 이름을 입력해주세요", 3000);
+    showAlert(".alert", "제대로된 이름을 입력해주세요", 2000);
     return;
   }
 
@@ -25,4 +29,18 @@ function clickSubmitHandler(e) {
   insertLast(resultArea, pick);
 }
 
+function clickCopyHandler(e) {
+  let text = resultArea.textContent;
+  // console.log("copy(text) : ", copy(text));
+  copy(text).then(() => {
+    showAlert(".alert-success", "클립보드 복사가 완료됐습니다.", 2000);
+  });
+
+  // 약속구문
+
+  // 약속
+  // 다음 해야 할 일
+}
+
 submit.addEventListener("click", clickSubmitHandler);
+resultArea.addEventListener("click", clickCopyHandler);
