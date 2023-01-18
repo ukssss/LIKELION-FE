bd443781fad1fdb7ae1368b3024d57acbab16733
ukssss <1872003115su@gmail.com>
2023-01-18 22:49:40 +0900

    20230118) delayP 함수를 생성하여 실행 지연 구현

--- Ajax/client/lib/utils/delay.js
+++ Ajax/client/lib/utils/delay.js
@@ -1,4 +1,5 @@
 import { getNode } from "../dom/getNode.js";
+import { isNumber, isObject } from "./typeOf.js";
 
 const first = getNode(".first");
 
@@ -10,12 +11,37 @@ function delay(callback, timeout = 1000) {
 // first.style.transform = 'rotate(360deg)';
 // first.style.top = '0px';
 
-delay(() => {
-  first.style.top = "-100px";
-  delay(() => {
-    delay(() => {
-      first.style.top = "0px";
-    });
+// delay(() => {
+//   first.style.top = "-100px";
+//   delay(() => {
+//     delay(() => {
+//       first.style.top = "0px";
+//     });
+//     first.style.transform = "rotate(360deg)";
+//   });
+// });
+
+delayP()
+  .then(() => {
+    first.style.top = "-100px";
+    return delayP(); // 1초 지연
+  })
+  .then(() => {
     first.style.transform = "rotate(360deg)";
+    return delayP(); // 1초 지연
+  })
+  .then(() => {
+    first.style.top = "0px";
   });
-});
+
+//* 실행을 1초 지연시키는 함수 생성
+function delayP(timeout = 1000) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("성공!");
+      // reject("실패!");
+    }, timeout);
+  });
+}
+
+console.log(delayP());
