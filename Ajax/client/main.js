b7ac6eba32a1463f9eb9f33f4dde2791a239f6a6
ukssss <1872003115su@gmail.com>
2023-01-20 01:44:22 +0900

    20230119) spinner 생성 및 렌더링

--- Ajax/client/main.js
+++ Ajax/client/main.js
@@ -1,4 +1,6 @@
-import { insertLast, xhrData, xhrPromise, ukss, delayP, getNode, renderUserCard } from "./lib/index.js";
+/* global gsap */
+
+import { insertLast, xhrData, xhrPromise, ukss, delayP, getNode, renderUserCard, renderSpinner, changeColor } from "./lib/index.js";
 
 // xhrPromise
 //   .get("https://jsonplaceholder.typicode.com/users/1")
@@ -34,12 +36,30 @@ import { insertLast, xhrData, xhrPromise, ukss, delayP, getNode, renderUserCard 
 const userCardContainer = getNode(".user-card-inner");
 
 async function rendingUserList() {
-  let response = await ukss.get("https://jsonplaceholder.typicode.com/users");
-  let userData = response.data;
+  renderSpinner(userCardContainer);
 
-  userData.forEach((data) => {
-    renderUserCard(userCardContainer, data);
-  });
+  try {
+    await delayP(2000);
+    getNode(".loadingSpinner").remove();
+
+    let response = await ukss.get("https://jsonplaceholder.typicode.com/users");
+    let userData = response.data;
+
+    userData.forEach((data) => {
+      renderUserCard(userCardContainer, data);
+    });
+
+    changeColor(".user-card");
+
+    gsap.to(gsap.utils.toArray(".user-card"), {
+      x: 0,
+      opacity: 1,
+      duration: 1.5,
+      stagger: 0.2,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 rendingUserList();
