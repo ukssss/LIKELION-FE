fe0af23a3ec0e0364dba3074dea35e52fd4e1d27
ukssss <1872003115su@gmail.com>
2023-01-19 16:14:05 +0900

    20230119) Storage 입력창 저장기능 구현

--- Ajax/client/main.js
+++ Ajax/client/main.js
@@ -1,1 +1,14 @@
-import {} from "./lib/index.js";
+import { getNode as $, loadStorage, saveStorage } from "./lib/index.js";
+
+const textField = $("#textField");
+const deleteButton = $("input[value='삭제']");
+
+loadStorage("area").then((res) => {
+  textField.value = res;
+});
+
+function inputHandler() {
+  saveStorage("area", textField.value);
+}
+
+textField.addEventListener("input", inputHandler);
