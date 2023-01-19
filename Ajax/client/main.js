8d7c8f71c7bbafa363411ffd374c86edcbf3dd6d
ukssss <1872003115su@gmail.com>
2023-01-19 15:39:04 +0900

    20230119) saveStorage 함수 제작

--- Ajax/client/main.js
+++ Ajax/client/main.js
@@ -1,11 +1,1 @@
-import { xhrData, insertLast } from "./lib/index.js";
-
-// xhrData.get(
-//   "https://jsonplaceholder.typicode.com/users/1",
-//   (res) => {
-//     insertLast("body", JSON.stringify(res));
-//   },
-//   (err) => {
-//     insertLast("body", "데이터 로딩에 실패했습니다.");
-//   }
-// );
+import {} from "./lib/index.js";
