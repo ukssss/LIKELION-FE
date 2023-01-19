4f2d0f1588d7c7a4bd48d6aaab66da67df9d55b9
ukssss <1872003115su@gmail.com>
2023-01-19 10:23:03 +0900

    20230119) createUserCard 함수 생성 및 렌더링

--- Ajax/client/main.js
+++ Ajax/client/main.js
@@ -1,10 +1,16 @@
-import { xhrData, insertLast, xhrPromise, tiger, delayP } from "./lib/index.js";
+import { insertLast, tiger, delayP, createUserCard, getNode } from "./lib/index.js";
+
+// rendingUserList 함수 만들기
+// ajax (tiger) get user list
+
+const userCardContainer = getNode(".user-card-inner");
 
 async function rendingUserList() {
-  let response = await tiger.get("https://jsonplaceholder.typicode.com/users");
+  let response = await tiger.get("https://jsonplaceholder.typicode.com/users/1");
 
   let userData = response.data;
-  console.log(userData);
+
+  insertLast(userCardContainer, createUserCard(userData));
 }
 
 rendingUserList();
