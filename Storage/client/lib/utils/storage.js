1710fb9a46b32517021eae70e562ad414311ac4c
ukssss <1872003115su@gmail.com>
2023-01-20 18:23:41 +0900

    20230120) loadStorage, deleteStorage 생성

--- Storage/client/lib/utils/storage.js
+++ Storage/client/lib/utils/storage.js
@@ -38,10 +38,10 @@ const albums = [
 // serialize()
 // deserialize()
 
-function saveStorage(key, value) {
+export function saveStorage(key, value) {
   return new Promise((resolve, reject) => {
     if (isString(key)) {
-      storage.setItem(key, value);
+      storage.setItem(key, serialize(value));
       resolve();
     } else {
       reject({ message: "key는 문자 타입 이어야 합니다." });
@@ -49,4 +49,20 @@ function saveStorage(key, value) {
   });
 }
 
-saveStorage("name", "ukss");
+export function loadStorage(key, value) {
+  return new Promise((resolve, reject) => {
+    if (isString(key)) {
+      resolve(deserialize(storage.getItem(key)));
+    } else {
+      reject({ message: "key는 문자 타입 이어야 합니다." });
+    }
+  });
+}
+
+export function deleteStorage(key, value) {
+  return new Promise((resolve, reject) => {
+    !key ? storage.clear() : storage.removeItem(key);
+    resolve();
+  });
+}
+
