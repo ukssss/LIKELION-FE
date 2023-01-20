69e6f96c92a8f503898e6db2c18dd06ad932453d
ukssss <1872003115su@gmail.com>
2023-01-20 20:37:44 +0900

    20230120) Storage input, load, delete 구현

--- Storage/client/main.js
+++ Storage/client/main.js
@@ -1,1 +1,22 @@
-import {} from "./lib/index.js";
+import { getNode, loadStorage, saveStorage, deleteStorage } from "./lib/index.js";
+
+const textField = getNode("#textField");
+const deleteBtn = getNode(".buttons > input:nth-child(2)");
+
+// 새로고침할 경우 Storage 에서 key 값이 "area" 인 데이터 textField 에 출력
+loadStorage("area").then((res) => {
+  textField.value = res;
+});
+
+// textField 에 값을 입력하면 Storage 에 key 값이 "area" 인 데이터 저장
+function inputHandler(e) {
+  saveStorage("area", textField.value);
+}
+
+// Storage 에 저장된 key 값이 "area" 인 데이터 삭제
+function deleteHandler(e) {
+  deleteStorage("area", textField.value);
+}
+
+textField.addEventListener("input", inputHandler);
+deleteBtn.addEventListener("click", deleteHandler);
