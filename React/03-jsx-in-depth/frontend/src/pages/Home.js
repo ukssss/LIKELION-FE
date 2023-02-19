02cc1edfaf3ab15f1e79a46ebc28985fdefe8a5a
ukssss <1872003115su@gmail.com>
2023-02-19 22:46:00 +0900

    03-jsx-in-depth | add, likeLionMember 데이터 출력

--- React/03-jsx-in-depth/frontend/src/pages/Home.js
+++ React/03-jsx-in-depth/frontend/src/pages/Home.js
@@ -1,42 +1,28 @@
+import { likeLionMembers } from '../data/likeLionMembers.js';
+
 class Home extends React.Component {
   state = {
-    products: [
-      {
-        id: 'product-dkfj',
-        name: '웰치스',
-        price: 980,
-        image: 'https://img.danawa.com/prod_img/500000/648/889/img/16889648_1.jpg?shrink=500:500&_v=20221021170923',
-      },
-      {
-        id: 'product-ekdi',
-        name: '두부 1kg',
-        price: 2180,
-        image: 'https://contents.lotteon.com/itemimage/_v105312/LF/14/37/17/3_/0/LF1437173_0_2.jpg/dims/resizef/720X720',
-      },
-    ],
+    members: likeLionMembers,
+  };
+
+  handleFilterLab = () => {
+    console.log('filtering');
   };
 
   render() {
-    const { products } = this.state;
-
-    const figcaptionStyle = {
-      display: 'flex',
-      gap: 12,
-    };
-
     return (
       <React.Fragment>
-        <h2>리스트 렌더링</h2>
+        <h2>멋쟁이 사자처럼 프론트엔드 스쿨 4기 멤버</h2>
+        <button type='button' style={{ marginBottom: 10 }} onClick={this.handleFilterLab}>
+          10조 모여!
+        </button>
         <ul>
-          {products.map(({ id, name, price, image }) => (
+          {this.state.members.map(({ id, name, gender }) => (
             <li key={id}>
-              <figure>
-                <img src={image} height={100} alt='' />
-                <figcaption style={figcaptionStyle}>
-                  <span>{name}</span>
-                  <span>{price}</span>
-                </figcaption>
-              </figure>
+              <p>
+                <span>{gender.includes('여성') ? '🙆🏻‍♀️' : '🙆🏻‍♂️'}</span>
+                <span>{name}</span>
+              </p>
             </li>
           ))}
         </ul>
