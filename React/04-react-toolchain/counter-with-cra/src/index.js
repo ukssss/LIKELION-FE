9f9217cc4eafd47e6698899c5db3882e5787b55c
ukssss <1872003115su@gmail.com>
2023-02-20 14:51:39 +0900

    04-react-toolchain | learn, React 툴체인 학습
    - CRA (다 제공)
    - Vite (기본적인 것만 제공)

--- React/04-react-toolchain/counter-with-cra/src/index.js
+++ React/04-react-toolchain/counter-with-cra/src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import { StrictMode } from 'react';
+
+// React v17 <- Internet Explorer 지원해야 할 경우 사용
+// React v18 <- 우리는 이걸로 사용
+import { createRoot } from 'react-dom/client';
+
+// Webpack 모듈 번들러
+// 스크립트 확장자 생략(.js, .jsx, .ts, .tsx, .json, .wasm)
+
+// Block__Element--Modifier
+
+import './styles/global.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
